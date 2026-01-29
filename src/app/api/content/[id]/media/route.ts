71f571058632b2eb72e1321796ea503bb1adb042
VladimirChudaev <vlchudaev@gmail.com>
2026-01-29 19:52:16 +0500

    updating content/route.tsx

--- src/app/api/content/[id]/media/route.ts
+++ src/app/api/content/[id]/media/route.ts
@@ -1,14 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-type Params = {
-  id: string;
-};
-
-export async function GET(
-  _req: NextRequest,
-  context: { params: Promise<Params> }
-) {
-  const { id } = await context.params;
-
+export async function GET(_req: NextRequest) {
   return NextResponse.json({ data: null });
 }
+
